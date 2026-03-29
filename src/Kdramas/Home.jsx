207c2d7ea574ecd5cdbd027e953ac677f2509a53
sharifjanof <azwebdanater@gmail.com>
2026-03-29 15:12:50 +0500

    updated

--- src/Kdramas/Home.jsx
+++ src/Kdramas/Home.jsx
@@ -1,17 +1,16 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Container } from "@mui/material";
 import DramaList from "./DramaList";
 import HeroBanner from "../Components/HeroBanner";
-import banner from "./reklama.jpeg";
 import taqlid from "./taqlid.jpg"
-import nefrit from "./nefritortidan.jpg"
-import oyogusidagiishq from "./Oy og'usidagi ishq.jpg"
-import { useStoreState } from "../Redux/selector";
-import locale from "../localization/locale.json";
+import nefrit from "./nefritortidan.jpeg"
+import oy from "./1765210455_oy-ogushidagi-ishq-s.jpg"
+// import { useStoreState } from "../Redux/selector";
+// import locale from "../localization/locale.json";
 
 export default function HomePage() {
-    const states = useStoreState();
-    const langData = useMemo(() => locale[states.lang], [states.lang]);
+    // const states = useStoreState();
+    // const langData = useMemo(() => locale[states.lang], [states.lang]);
     const banners = [
         {
             imageUrl: nefrit,
@@ -24,7 +23,7 @@ export default function HomePage() {
             subtitle: "Premyera: Taqldi 1-qism",
         },
         {
-            imageUrl: oyogusidagiishq,
+            imageUrl: oy,
             title: "oy og'usidagi ishq",
             subtitle: "Premyera: Oy og'usidagi ishq 1-qism",
         },
